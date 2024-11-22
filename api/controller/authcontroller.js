3c344bf1186172385fc67a7015944068d89157b7
John-Rep <replogle.john@next-u.fr>
2024-11-22 12:04:05 +0100

    feat: Ajout du OAuth2, mais un souci avec authentification de GitHub

--- api/controller/authcontroller.js
+++ api/controller/authcontroller.js
@@ -2,6 +2,11 @@ const User = require("../models/user");
 var bcrypt = require("bcryptjs");
 const config = require("../config/key");
 var jwt = require("jsonwebtoken");
+const axios = require("axios");
+
+const CLIENT_ID = "Ov23liUpmQ2JuEaC1HKD";
+const CLIENT_SECRET = "815fe7e64816840dc83d10c32d8680f0c1164d64";
+const GITHUB_URL = "https://github.com/login/oauth/access_token";
 
 exports.signup = async (req, res) => {
     const user = new User({
@@ -60,3 +65,52 @@ exports.signin = async (req, res) => {
     });
   };
   
+  exports.oauth2Redirect = async (req, res) => {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: `${GITHUB_URL}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`,
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      const userData = await axios({
+        method: "GET",
+        url: "https://api.github.com/user",
+        headers: {
+          Authorization: "token " + response.data.access_token,
+        },
+      });
+      console.log(userData);
+      const { login, name } = userData.data;
+      let user = await User.findOne({ login }).select("-password");
+      if (!user) {
+         user = new User({
+          username: login,
+          name: name,
+        });
+        try {
+          await user.save();
+        } catch (err) {
+          console.log(err);
+          res.status(500).send("Erreur lors de la création de compte");
+        }
+      }
+      const token = jwt.sign(
+        { id: user.id, username: user.username },
+        config.secret,
+        {
+          algorithm: "HS256",
+          allowInsecureKeySizes: true,
+          expiresIn: 86400, // 24 hours
+        }
+      );
+  
+      res.redirect(
+        `http://localhost:8080/todo.html?access_token=${token}`
+      );
+    } catch (error) {
+      console.error("Erreur lors de l'authentification GitHub :", error);
+      res.status(500).send("Erreur lors de l'authentification GitHub");
+    }
+  };
