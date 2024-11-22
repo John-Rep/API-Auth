a1e4d8641efca5d7e10d527f4ae953243ceb1809
John-Rep <replogle.john@next-u.fr>
2024-11-22 11:28:14 +0100

    feat: Admin rateLimit

--- api/middlewares/rateLimiter.js
+++ api/middlewares/rateLimiter.js
@@ -1,10 +1,31 @@
 const setRateLimit = require("express-rate-limit");
+const User = require("../models/user.js");
 
-const rateLimitMiddleware = setRateLimit({
+
+
+rateLimiter = async (req, res, next) => {
+    const user = await User.findById(req.userId);
+    if (user.admin) {
+        return adminLimiter(req, res, next);
+    } else {
+        return userLimiter(req, res, next);
+    }
+  };
+
+
+const userLimiter = setRateLimit({
+        windowMs: 60 * 1000,
+        max: 5,
+        message: "You have exceeded your 5 requests per minute limit.",
+        headers: true,
+})
+
+const adminLimiter = setRateLimit({
     windowMs: 60 * 1000,
-    max: 5,
-    message: "You have exceeded your 5 requests per minute limit.",
+    max: 10,
+    message: "You have exceeded your 10 requests per minute limit.",
     headers: true,
 })
 
-module.exports = rateLimitMiddleware;
+
+module.exports = rateLimiter;
